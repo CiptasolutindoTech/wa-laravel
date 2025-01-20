07bd89bd1dcd82adff608ed0c0d38071962b8d95
devlagret <sioputra4@gmail.com>
2025-01-20 16:56:12 +0700

    Bug fix

--- src/lib/RuangWa.php
+++ src/lib/RuangWa.php
@@ -8,7 +8,7 @@ class RuangWa extends Connection {
     * Get the value of driver
     */
     public function url(){
-        if($this->serverUrl??empty(config("wa.ruang_wa_url"))){
+        if(empty($this->serverUrl??config("wa.ruang_wa_url"))){
             throw new \Exception('Whatsapp api url cant be empty');
         }
         return $this->serverUrl??config("wa.ruang_wa_url");
